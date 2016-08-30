8f415fc0db97778e282e344ed942bb182e8db85b
Gaurav Pathak <CaRa@aas-MacBook-Pro.local>
2016-08-30 19:11:24 +0530

    Added the batch normalisation layers, works magically

--- assignment2/cs231n/classifiers/cnn.py
+++ assignment2/cs231n/classifiers/cnn.py
@@ -11,7 +11,7 @@ class ThreeLayerConvNet(object):
   conv - relu - 2x2 max pool - affine - relu - affine - softmax
   
   The network operates on minibatches of data that have shape (N, C, H, W)
-  consisting of N images, each with height H and width W and with C input
+   consisting of N images, each with height H and width W and with C input
   channels.
   """
   
@@ -48,10 +48,21 @@ class ThreeLayerConvNet(object):
     ############################################################################
     self.params['W1'] = (2.0/np.product(iDim)) * np.random.randn(num_filters, input_dim[0],filter_size, filter_size)
     self.params['b1'] = np.zeros(num_filters)
+    self.params['gamma1'] = np.ones(num_filters)
+    self.params['beta1'] = np.zeros(num_filters)
+
+    # for the second convolution layer
     self.params['W2'] = (2.0/(num_filters*filter_size*filter_size)) * np.random.randn(num_filters, num_filters, filter_size, filter_size)
     self.params['b2'] = np.zeros(num_filters)
+    self.params['gamma2'] = np.ones(num_filters)
+    self.params['beta2'] = np.zeros(num_filters)
+
+    # for the fully connected layer
     self.params['W3'] = (2.0/(num_filters*(iDim[1]/4)*(iDim[1]/4))) * np.random.randn(hidden_dim, num_filters, input_dim[1]/4, input_dim[1]/4)
     self.params['b3'] = np.zeros(hidden_dim)
+    self.params['gamma3'] = np.ones(hidden_dim)
+    self.params['beta3'] = np.zeros(hidden_dim)
+
     self.params['W4'] = (2.0/hidden_dim) * np.random.randn(num_classes, hidden_dim, 1,1)
     self.params['b4'] = np.zeros(num_classes)
     ############################################################################
@@ -69,9 +80,13 @@ class ThreeLayerConvNet(object):
     Input / output: Same API as TwoLayerNet in fc_net.py.
     """
     W1, b1 = self.params['W1'], self.params['b1']
+    gamma1, beta1 = self.params['gamma1'], self.params['beta1']
     W2, b2 = self.params['W2'], self.params['b2']
+    gamma2, beta2 = self.params['gamma2'], self.params['beta2']
     W3, b3 = self.params['W3'], self.params['b3']
+    gamma3, beta3 = self.params['gamma3'], self.params['beta3']
     W4, b4 = self.params['W4'], self.params['b4']
+
     # pass conv_param to the forward pass for the convolutional layer
     filter_size = W1.shape[2]
     conv_param = {'stride': 1, 'pad': (filter_size - 1) / 2}
@@ -79,6 +94,24 @@ class ThreeLayerConvNet(object):
     dropout_param = {'p': 0.5, 'mode': 'train'}
     # pass pool_param to the forward pass for the max-pooling layer
     pool_param = {'pool_height': 2, 'pool_width': 2, 'stride': 2}
+    # will have to define separate bn_params dictionary for each layer
+    # since running mean and variance will be different for each layer
+    bn_params1 = bn_params2 = bn_params3 = {'mode':'train'}
+
+    # defining helper function and enclosing conv_bn_relu in one
+    def conv_bn_relu_f(X, W, b, gamma, beta, conv_param, bn_param):
+      a1, c1 = conv_forward_fast(X, W, b, conv_param)
+      a2, c2 = spatial_batchnorm_forward(a1, gamma, beta, bn_param)
+      out, c3 = relu_forward(a2)
+      cache = (c1, c2, c3)
+      return out, cache
+
+    def conv_bn_relu_b(dout, cache):
+      c1, c2, c3 = cache
+      da3 = relu_backward(dout, c3)
+      da2, dgamma, dbeta = spatial_batchnorm_backward(da3, c2)
+      da1, dW, db = conv_backward_fast(da2, c1)
+      return da1, dW, db, dgamma, dbeta 
 
     scores = None
     N = X.shape[0]
@@ -88,15 +121,21 @@ class ThreeLayerConvNet(object):
     # computing the class scores for X and storing them in the scores          #
     # variable.                                                                #
     ############################################################################
-    a1, c1 = conv_relu_pool_forward(X, W1, b1, conv_param, pool_param)
-    a2, c2 = conv_relu_pool_forward(a1, W2, b2, conv_param, pool_param)
-    a3, c3 = conv_relu_forward(a2, W3, b3, fc_param)
-    #if y == None:
-    # dropout_param['mode'] = 'test'
-    #d1, cd1 = dropout_forward(a3, dropout_param)
-    a4, c4 = conv_forward_fast(a3, W4, b4, fc_param)
-    a4 = a4.reshape(N,num_classes)
-    scores = a4
+    a1, c1 = conv_bn_relu_f(X, W1, b1, gamma1, beta1, conv_param, bn_params1)
+    a2, c2 = max_pool_forward_fast(a1, pool_param)
+
+    # second layer convolution and max pool
+    a3, c3 = conv_bn_relu_f(a2, W2, b2, gamma2, beta2, conv_param, bn_params2)
+    a4, c4 = max_pool_forward_fast(a3, pool_param)
+
+    # for the fully connected layer
+    a5, c5 = conv_bn_relu_f(a4, W3, b3, gamma3, beta3, fc_param, bn_params3)
+
+    # for the output layer
+    a6, c6 = conv_forward_fast(a5, W4, b4, fc_param)
+    a6 = a6.reshape(N, num_classes)
+    
+    scores = a6
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
@@ -114,13 +153,16 @@ class ThreeLayerConvNet(object):
     loss, d_softmax = softmax_loss(scores, y)
     loss += 0.5 * self.reg * (np.sum(W1*W1) + np.sum(W2*W2) + np.sum(W3*W3)+ np.sum(W4*W4))
     d_softmax = d_softmax.reshape(N,num_classes,1,1)
-    da4, dw4, db4 = conv_backward_fast(d_softmax, c4)
+    da6, dw4, db4 = conv_backward_fast(d_softmax, c6)
     # if y == None:
     #   dropout_param['mode'] = 'test'
     # dd1 = dropout_backward(da4, cd1)
-    da3, dw3, db3 = conv_relu_backward(da4, c3)
-    da2, dw2, db2 = conv_relu_pool_backward(da3, c2)
-    da1, dw1, db1 = conv_relu_pool_backward(da2, c1)
+    da5, dw3, db3, dgamma3, dbeta3 = conv_bn_relu_b(da6, c5)
+    da4 = max_pool_backward_fast(da5, c4)
+    da3, dw2, db2, dgamma2, dbeta2 = conv_bn_relu_b(da4, c3)
+    da2 = max_pool_backward_fast(da3, c2)
+    da1, dw1, db1, dgamma1, dbeta1 = conv_bn_relu_b(da2, c1)
+
     grads['W4'] = dw4 + self.reg*W4
     grads['b4'] = db4
     grads['W3'] = dw3 + self.reg*W3
@@ -129,6 +171,12 @@ class ThreeLayerConvNet(object):
     grads['b2'] = db2
     grads['W1'] = dw1 + self.reg*W1
     grads['b1'] = db1
+    grads['gamma1'] = dgamma1
+    grads['gamma2'] = dgamma2
+    grads['gamma3'] = dgamma3
+    grads['beta1'] = dbeta1
+    grads['beta2'] = dbeta2
+    grads['beta3'] = dbeta3
     ############################################################################
     #                             END OF YOUR CODE                             #
     ############################################################################
